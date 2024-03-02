ab3b162dbc38f5be78040a3739fb7089069e3c86
Awais68 <161300531+Awais68@users.noreply.github.com>
2024-03-02 15:02:40 +0500

    Arthmatic operators and concatinate

---
+++ ArthmaticOperators/main.js
@@ -0,0 +1,34 @@
+//jiske aage ++/-- lagega wohi variable update hoga.
+// post main pehle variable ko value assign hogi phr update hogi.
+// pre main pehle variable update hoga phr value assign hogi.
+// var pNum = 10
+// var pNewNum = pNum++
+
+// console.log(pNum)
+// console.log(pNewNum)
+
+//post decrement
+var pdNum = 10
+var pdNewNum = pdNum--
+
+console.log(pdNum)
+console.log(pdNewNum)
+
+//pre increment
+var prNum = 10
+var prNewNum = ++prNum
+console.log(prNum)
+console.log(prNewNum)
+
+//pre decrement
+var prdNum = 10
+var prdNewNum = --prdNum
+console.log(prdNum)
+console.log(prdNewNum)
+
+var abc = 10
+
+console.log(abc++)
+console.log(abc++)
+console.log(abc)
+console.log(abc)
---
+++ concatenatingStrings/app.js
@@ -0,0 +1,51 @@
+
+
+var firstName = 'Awais'
+var lastName = 'Niaz'
+
+var fullName = firstName + ' '+ lastName
+
+console.log(fullName)
+// document.write ('<h1>' + "Salaam" + ' ' + fullName +  '!</h1>')
+// document.write(`<h1> Salaam ${fullName} </h1>`)
+
+//Template Strings
+//ye use krte hen Back-Tics ``
+var fName = `Salaam ${firstName} ${lastName}`
+
+var tableNumber = 4
+var tNum = 1
+var tdNum = 1
+
+document.write(`<h5> ${tableNumber} Ka Table </h5>`)
+document.write(`<li> ${tableNumber} x ${tdNum} = ${tableNumber * tNum} </li>`)
+document.write(`<li> ${tableNumber} x ${++tdNum} = ${tableNumber * ++tNum} </li>`)
+document.write(`<li> ${tableNumber} x ${++tdNum} = ${tableNumber * ++tNum} </li>`)
+document.write(`<li> ${tableNumber} x ${++tdNum} = ${tableNumber * ++tNum} </li>`)
+document.write(`<li> ${tableNumber} x ${++tdNum} = ${tableNumber * ++tNum} </li>`)
+document.write(`<li> ${tableNumber} x ${++tdNum} = ${tableNumber * ++tNum} </li>`)
+document.write(`<li> ${tableNumber} x ${++tdNum} = ${tableNumber * ++tNum} </li>`)
+document.write(`<li> ${tableNumber} x ${++tdNum} = ${tableNumber * ++tNum} </li>`)
+document.write(`<li> ${tableNumber} x ${++tdNum} = ${tableNumber * ++tNum} </li>`)
+document.write(`<li> ${tableNumber} x ${++tdNum} = ${tableNumber * ++tNum} </li>`)
+document.write(`<li> ${tableNumber} x ${++tdNum} = ${tableNumber * ++tNum} </li>`)
+document.write(`<li> ${tableNumber} x ${++tdNum} = ${tableNumber * ++tNum} </li>`)
+
+
+var tableNumber = 12
+var tNum = 11
+var tdNum = 11
+
+document.write(`<h5> ${tableNumber} Ka Table </h5>`)
+document.write(`<li> ${tableNumber} x ${tdNum} = ${tableNumber * tNum} </li>`)
+document.write(`<li> ${tableNumber} x ${++tdNum} = ${tableNumber * ++tNum} </li>`)
+document.write(`<li> ${tableNumber} x ${++tdNum} = ${tableNumber * ++tNum} </li>`)
+document.write(`<li> ${tableNumber} x ${++tdNum} = ${tableNumber * ++tNum} </li>`)
+document.write(`<li> ${tableNumber} x ${++tdNum} = ${tableNumber * ++tNum} </li>`)
+document.write(`<li> ${tableNumber} x ${++tdNum} = ${tableNumber * ++tNum} </li>`)
+document.write(`<li> ${tableNumber} x ${++tdNum} = ${tableNumber * ++tNum} </li>`)
+document.write(`<li> ${tableNumber} x ${++tdNum} = ${tableNumber * ++tNum} </li>`)
+document.write(`<li> ${tableNumber} x ${++tdNum} = ${tableNumber * ++tNum} </li>`)
+document.write(`<li> ${tableNumber} x ${++tdNum} = ${tableNumber * ++tNum} </li>`)
+document.write(`<li> ${tableNumber} x ${++tdNum} = ${tableNumber * ++tNum} </li>`)
+document.write(`<li> ${tableNumber} x ${++tdNum} = ${tableNumber * ++tNum} </li>`)
