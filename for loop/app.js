48c9b980e7c3f0c740dd881750d9024788915ee2
Mr. Awais <161300531+Awais68@users.noreply.github.com>
2024-03-23 00:30:02 -0700

    update for loop.js

--- for loop/app.js
+++ for loop/app.js
@@ -1,19 +1,19 @@
 //   20 ka table banana hai
-// var tableNumber = prompt(" Table Number")
-// for (var i = 1; i <= 100; i++){
-//     document.write( tableNumber +' x ' + i +' = ' + tableNumber * i  + "<br>")
-// }
+var tableNumber = prompt(" Table Number")
+for (var i = 1; i <= 100; i++){
+    document.write( tableNumber +' x ' + i +' = ' + tableNumber * i  + "<br>")
+}
 
-// var weekDays = ['Mon', 'Tue', 'Wed', 'Thurs', 'Fri', 'sat', 'sun']
+var weekDays = ['Mon', 'Tue', 'Wed', 'Thurs', 'Fri', 'sat', 'sun']
 
-// document.write(weekDays[0])
-// document.write(weekDays[1])
-// document.write(weekDays[2])
-// document.write(weekDays[3])
+document.write(weekDays[0])
+document.write(weekDays[1])
+document.write(weekDays[2])
+document.write(weekDays[3])
 
-// for(var i = 0; i < weekDays.length; i++ ) {
-//     document.write(weekDays[i] +"<br>")
-// }
+for(var i = 0; i < weekDays.length; i++ ) {
+    document.write(weekDays[i] +"<br>")
+}
 
 for (var Tawaf = 1; Tawaf <= 7; Tawaf++) {
     document.write(Tawaf + ' Chakkar' + "<br>")
